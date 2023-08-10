d1492b2acbc602e68d823018565e78545e137538
Niki42 <nik.7377@gmail.com>
2023-08-10 18:34:21 +0200

    same some lines

--- cfiles/main.c
+++ cfiles/main.c
@@ -8,10 +8,11 @@ void init_data( t_data *data )
 	data->win_ptr = NULL;
 }
 
-void free_data(t_data *data)
+int free_data(t_data *data)
 {
 	if(data->map)
 		free2d(data->map);
+	return (1);
 }
 
 int	main(int argc, char **argv)
@@ -22,10 +23,7 @@ int	main(int argc, char **argv)
 	if (input_handle(argc, argv))
 		return (1);
 	if (map_check( &data.map, argv[1]) || window_loop( &data ))
-	{
-		free_data(&data);
-		return (1);
-	}
+		return (free_data(&data));
 	free_data(&data);
 	return (0);
 }
