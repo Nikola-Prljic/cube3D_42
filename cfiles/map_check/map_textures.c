e598d9458b23f7ff12c5386e97a7ab2446a033e1
Niki42 <nik.7377@gmail.com>
2023-08-14 17:11:50 +0200

    fixed double free, with adding ft_strdub

--- cfiles/map_check/map_textures.c
+++ cfiles/map_check/map_textures.c
@@ -3,10 +3,10 @@
 #include "ft_getline/ft_getline.h"
 
 //need to replace with open xpm texture!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! <----
-char *open_textures(char *texture_path)
+/* char *open_textures(char *texture_path)
 {
 	return texture_path;
-}
+} */
 
 int free_texture_lines(char **texture_path, char **direction)
 {
@@ -25,13 +25,13 @@ int is_possible_direction(t_data *data, t_map *file)
 	direction = ft_getline(file->fd, &file->buffer, ' ');
 	texture_path = ft_getline(file->fd, &file->buffer, '\n');
 	if(!ft_strcmp(direction, "NO"))
-		data->texture->north = open_textures(texture_path);
+		data->texture->north = ft_strdup(texture_path);
 	else if(!ft_strcmp(direction, "SO"))
-		data->texture->south = open_textures(texture_path);
+		data->texture->south = ft_strdup(texture_path);
 	else if(!ft_strcmp(direction, "WE"))
-		data->texture->west = open_textures(texture_path);
+		data->texture->west = ft_strdup(texture_path);
 	else if(!ft_strcmp(direction, "EA"))
-		data->texture->east = open_textures(texture_path);
+		data->texture->east = ft_strdup(texture_path);
 	else
 		return(free_texture_lines(&texture_path, &direction));
 	free_texture_lines(&texture_path, &direction);
