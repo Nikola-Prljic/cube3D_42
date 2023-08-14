08d8d089a05eb4c45dbc2bb85c2e6ee0afa7e385
R1ch1no <richard.kurnava@gmail.com>
2023-08-14 17:04:32 +0200

    before merge

--- raycasting/raylen.c
+++ raycasting/raylen.c
@@ -4,11 +4,15 @@
 void	raylen(t_data *data, float rd, int *re)
 {
 	int	rx;
+	int	rx_2;
 	int	ry;
+	int	ry_2;
 
 	(void)re;
 	(void)rd;
-	rx = data->rays->player_x - data->rays->player_dir_x * 5;
-	ry = data->rays->player_y - data->rays->player_dir_y * 5; 
+	rx_2 = data->rays->player_x;
+	ry_2 = data->rays->player_y;
+	rx = rx_2 - data->rays->player_dir_x * 5;
+	ry = ry_2 - data->rays->player_dir_y * 5;
 	mlx_pixel_put(data->mlx_ptr, data->win_ptr, rx, ry, 0x00FF0000);
 }
