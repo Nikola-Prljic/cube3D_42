352de6baf00e35eb4ffa4cf6f7ef43a7cf18d8d0
R1ch1no <richard.kurnava@gmail.com>
2023-08-24 13:09:19 +0200

    huh

--- raycasting/raylen.c
+++ raycasting/raylen.c
@@ -6,11 +6,19 @@
 /*   By: rkurnava <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 19:24:19 by rkurnava          #+#    #+#             */
-/*   Updated: 2023/08/24 12:17:22 by rkurnava         ###   ########.fr       */
+/*   Updated: 2023/08/24 13:03:27 by rkurnava         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
+
+int	hit(char **map, int ay, int ax)
+{
+	if (ay > 0 && ay < WINDOW_HEIGT && ax > 0 && ax < WINDOW_WITH && map[ay
+		/ 64][ax / 64] != '1')
+		return (1);
+	return (0);
+}
 
 void	where_we_look(t_data *data, int reseting, float angle)
 {
@@ -20,18 +28,18 @@ void	where_we_look(t_data *data, int reseting, float angle)
 		data->rays->up_down = 0;
 		return ;
 	}
-	if ((int)rad2deg(angle) > 0 && (int)rad2deg(angle) < 180)
-		data->rays->up_down = 1;
-	else if ((int)rad2deg(angle) > 180 && (int)rad2deg(angle) < 359)
-		data->rays->up_down = -1;
-	else
+	if ((int)rad2deg(angle) == 0 && (int)rad2deg(angle) == 180)
 		data->rays->up_down = 0;
-	if ((int)rad2deg(angle) < 270 && (int)rad2deg(angle) > 90)
+	else if ((int)rad2deg(angle) > 0 && (int)rad2deg(angle) < 180)
+		data->rays->up_down = 1;
+	else if ((int)rad2deg(angle) > 180 && (int)rad2deg(angle) < 360)
+		data->rays->up_down = -1;
+	if ((int)rad2deg(angle) == 270 && (int)rad2deg(angle) == 90)
+		data->rays->left_right = 0;
+	else if ((int)rad2deg(angle) < 270 && (int)rad2deg(angle) > 90)
 		data->rays->left_right = -1;
 	else if ((int)rad2deg(angle) > 270 || (int)rad2deg(angle) < 90)
 		data->rays->left_right = 1;
-	else
-		data->rays->left_right = 0;
 }
 
 void	raylen_v(t_data *data, float angle, char **map)
@@ -53,8 +61,7 @@ void	raylen_v(t_data *data, float angle, char **map)
 		y_step *= -1;
 	if (data->rays->up_down == 1 && y_step < 0)
 		y_step *= -1;
-	while (ay > 0 && ay < WINDOW_HEIGT && ax > 0 && ax < WINDOW_WITH
-		&& map[ay / 64][ax / 64] != '1')
+	while (hit(map, ay, ax))
 	{
 		ax += x_step;
 		ay += y_step;
@@ -82,8 +89,7 @@ void	raylen_h(t_data *data, float angle, char **map)
 		x_step *= -1;
 	if (data->rays->left_right == 1 && x_step < 0)
 		x_step *= -1;
-	while (ay > 0 && ay < WINDOW_HEIGT && ax > 0 && ax < WINDOW_WITH
-		&& map[ay / 64][ax / 64] != '1')
+	while (hit(map, ay, ax))
 	{
 		ax += x_step;
 		ay += y_step;
@@ -97,12 +103,16 @@ void	raylen(t_data *data)
 	int		vl;
 	int		hl;
 	int		raycount;
-	float	ray_angle;
+	double	ray_angle;
 
 	raycount = 0;
 	ray_angle = data->rays->player_dir - (PI / 6);
 	while (raycount < WINDOW_HEIGT)
 	{
+		if (ray_angle > 2 * PI)
+			ray_angle -= deg2rad(360.0);
+		if (ray_angle < 0)
+			ray_angle += deg2rad(360.0);
 		where_we_look(data, 0, ray_angle);
 		raylen_h(data, ray_angle, data->map);
 		raylen_v(data, ray_angle, data->map);
@@ -112,10 +122,10 @@ void	raylen(t_data *data)
 					- data->rays->h_y, 2));
 		if (hl < vl)
 			mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
-				data->rays->space, data->rays->h_x, data->rays->h_y);
+					data->rays->space, data->rays->h_x, data->rays->h_y);
 		else
 			mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
-				data->rays->space, data->rays->v_x, data->rays->v_y);
+					data->rays->space, data->rays->v_x, data->rays->v_y);
 		raycount++;
 		ray_angle += (float)deg2rad(60.0 / (float)WINDOW_WITH);
 	}
