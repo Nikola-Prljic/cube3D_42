3a5b262badb3cec7312428f9e9070e43ed7babc0
Niki42 <nik.7377@gmail.com>
2023-08-31 16:37:44 +0200

    dsad

--- cfiles/window_loop.c
+++ cfiles/window_loop.c
@@ -86,11 +86,8 @@ int	renderCub(t_data *data)
 {
 	if (!data->mlx_ptr || !data->win_ptr /* || render_cub == STOP_RENDER */)
 		return (0);
-	printf("floor = %i\n", data->floor_rgb);
-	printf("sky = %i\n", data->sky_rgb);
 	draw_floor_sky(data, data->sky_rgb, data->floor_rgb);
 	raylen(data);
-	/* draw_minimap(data); */
 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->img, 0,
 		0);
 	return (1);
@@ -138,19 +135,6 @@ int	keypress(int keysum, t_data *data)
 		data->zoom_faktor--;
 	degrees = data->rays->player_dir * (180 / PI);
 	renderCub(data);
-	/* print_2d(data); */
-	/* mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->rays->player,
-			data->rays->px - 21.5, data->rays->py - 21.5); */
-	/* ft_rect(data, (t_rect){0, 0, WINDOW_HEIGT, WINDOW_WITH, 0x89CFF0});
-	raylen(data); */
-	// draw_texture(data);
-	/* mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_img,
-			0,
-		0); */
-	/* mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->north->img,
-			100,
-		0); */
-	/* data->render_cub == STOP_RENDER; */
 	return (1);
 }
 
@@ -178,23 +162,12 @@ int	window_loop(t_data *data)
 	create_img_addr(data, data->east);
 	create_img_addr(data, data->west);
 	create_img_addr(data, data->south);
-	/* ft_rect(data, (t_rect){200, 200, 50, 50, 0xFF0000}); */
-	/* prepare_player(data); */
-	/* print_2d(data); */
-	/* mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->rays->player,
-			data->rays->px - 21.5, data->rays->py - 21.5); */
 	renderCub(data);
 	mlx_hook(data->win_ptr, 17, 0, &x_close, data);
 	mlx_hook(data->win_ptr, KeyPress, KeyPressMask, &keypress, data);
-	/* mlx_loop_hook(data->mlx_ptr, &renderCub, data); */
 	mlx_loop(data->mlx_ptr);
-	/* mlx_destroy_image(data->mlx_ptr, data->rays->player);
-	mlx_destroy_image(data->mlx_ptr, data->rays->wall);
-	mlx_destroy_image(data->mlx_ptr, data->rays->space);
-	mlx_destroy_image(data->mlx_ptr, data->rays->spacev); */
 	if (data->rays)
 		free(data->rays);
-	// mlx_destroy_display(data->mlx_ptr);
 	free_data(data);
 	return (0);
 }
